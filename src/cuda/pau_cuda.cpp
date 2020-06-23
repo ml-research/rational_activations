6a75e3b4943301b4fe6642eca2a4b1250f46e3ba
Rudolf Lioutikov <lioutikov@utexas.edu>
2020-06-23 09:20:25 -0500

    added a pytorch check for cuda so that we gpu-less-laptop people can still install it ;). Also changed default value for the 'cuda' argument in the PAU class to whatever torch.cuda.is_available() returns

--- src/cuda/pau_cuda.cpp
+++ src/cuda/pau_cuda.cpp
@@ -8,19 +8,16 @@
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 
-
-
     
-	at::Tensor pau_cuda_forward_A_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
+    at::Tensor pau_cuda_forward_A_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
     std::vector<torch::Tensor> pau_cuda_backward_A_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d);
-    
-
+        
     
     at::Tensor pau_forward_A_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d) {
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
+    
         return pau_cuda_forward_A_5_4(x, n, d);
     }
     std::vector<torch::Tensor> pau_backward_A_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d) {
@@ -28,22 +25,20 @@
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
+    
         return pau_cuda_backward_A_5_4(grad_output, x, n, d);
     }
     
-
     
-	at::Tensor pau_cuda_forward_B_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
+    at::Tensor pau_cuda_forward_B_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
     std::vector<torch::Tensor> pau_cuda_backward_B_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d);
-    
-
+        
     
     at::Tensor pau_forward_B_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d) {
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
+    
         return pau_cuda_forward_B_5_4(x, n, d);
     }
     std::vector<torch::Tensor> pau_backward_B_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d) {
@@ -51,22 +46,20 @@
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
+    
         return pau_cuda_backward_B_5_4(grad_output, x, n, d);
     }
     
-
     
-	at::Tensor pau_cuda_forward_C_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
+    at::Tensor pau_cuda_forward_C_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
     std::vector<torch::Tensor> pau_cuda_backward_C_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d);
-    
-
+        
     
     at::Tensor pau_forward_C_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d) {
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
+    
         return pau_cuda_forward_C_5_4(x, n, d);
     }
     std::vector<torch::Tensor> pau_backward_C_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d) {
@@ -74,30 +67,29 @@
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
+    
         return pau_cuda_backward_C_5_4(grad_output, x, n, d);
     }
     
     
-	at::Tensor pau_cuda_forward_D_5_4(const bool training, const unsigned long long iteration, torch::Tensor x, torch::Tensor n, torch::Tensor d);
-    std::vector<torch::Tensor> pau_cuda_backward_D_5_4(const bool training, const unsigned long long iteration, torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d);
+    at::Tensor pau_cuda_forward_D_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d);
+    std::vector<torch::Tensor> pau_cuda_backward_D_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d);
+        
     
-
-    
-    at::Tensor pau_forward_D_5_4(const bool training, const unsigned long long iteration, torch::Tensor x, torch::Tensor n, torch::Tensor d) {
+    at::Tensor pau_forward_D_5_4(torch::Tensor x, torch::Tensor n, torch::Tensor d) {
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
-        return pau_cuda_forward_D_5_4(training, iteration, x, n, d);
+    
+        return pau_cuda_forward_D_5_4(x, n, d);
     }
-    std::vector<torch::Tensor> pau_backward_D_5_4(const bool training, const unsigned long long iteration, torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d) {
+    std::vector<torch::Tensor> pau_backward_D_5_4(torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d) {
         CHECK_INPUT(grad_output);
         CHECK_INPUT(x);
         CHECK_INPUT(n);
         CHECK_INPUT(d);
-
-        return pau_cuda_backward_D_5_4(training, iteration, grad_output, x, n, d);
+    
+        return pau_cuda_backward_D_5_4(grad_output, x, n, d);
     }
     
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
