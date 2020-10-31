44540488c7b9da163f66036816d3fba22bc52928
Quentin Delfosse <q.delfosse@gmail.com>
2020-10-31 17:55:07 +0100

    Trying to get pau in the conf.py of docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-from pau import __version__
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-
+sys.path.insert(0, os.path.abspath('../..'))
+from pau import __version__
 
 # -- Project information -----------------------------------------------------
 
