16a23ca061d4b5b4d3be01951a0222e62f04983a
Julius Zimmermann <zimmermann.julius@web.de>
2020-11-20 17:41:05 +0100

    add conversion method for pytorch models and precomputed functions;

--- rational/utils.py
+++ rational/utils.py
@@ -1,12 +1,14 @@
 import warnings
 import numpy as np
 from numpy import zeros, inf
+import torch.nn as nn
+from rational_torch import Rational
 from scipy.optimize.optimize import OptimizeWarning
 from scipy.optimize._lsq.least_squares import prepare_bounds
 from scipy.optimize.minpack import leastsq, _wrap_jac
 
 # np.random.seed(0)
-
+activations = {nn.ReLU: 'relu', nn.LeakyReLU: 'leaky_relu', nn.Tanh: 'tanh', nn.Sigmoid: 'sigmoid', nn.GELU: 'gelu', nn.Hardswish: 'swish'}
 
 def _wrap_func(func, xdata, ydata, degrees):
     def func_wrapped(params):
@@ -96,3 +98,29 @@ def find_closest_equivalent(rational_func, new_func, x):
     final_func_output = np.array(equivalent_func(x, a, b, c, d))
     final_distance = ((y - final_func_output)**2).sum()
     return (a, b, c, d), final_distance
+
+
+def convert_pytorch_model_to_rational(model, rational_version='A', rational_cuda=False):
+    converted = nn.Sequential()
+    for name, layer in model.named_children():
+        childs = layer.children()
+        if len(list(childs)) > 0:
+            sequential = nn.Sequential()
+            for n, l in layer.named_children():
+                sequential.add_module(*_convert_layer(n, l, version=rational_version, cuda=rational_cuda))
+            converted.add_module(name, sequential)
+        else:
+            converted.add_module(*_convert_layer(name, layer, version=rational_version, cuda=rational_cuda))
+    return converted
+        
+
+def _convert_layer(name, layer, version, cuda):
+    for activation in activations:
+        if isinstance(layer, activation):
+            return f'Rational_{name}', Rational(version=version, approx_func=activations[activation], cuda=cuda)
+    return name, layer
+        
+
+
+
+
