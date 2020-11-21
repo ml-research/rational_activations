cac52965f771cbf8e859b4d1e6893a6a4f2379f5
Julius Zimmermann <zimmermann.julius@web.de>
2020-11-21 16:02:01 +0100

    implement first versions of model conversion for mxnet and pytorch, add repr for mxnet

--- rational/utils.py
+++ rational/utils.py
@@ -2,7 +2,9 @@ import warnings
 import numpy as np
 from numpy import zeros, inf
 import torch.nn as nn
-from rational_torch import Rational
+from mxnet.gluon.nn import HybridSequential, Activation
+from rational_torch import Rational as RationalPyTorch
+from rational_mxnet import Rational as RationalMxNet
 from scipy.optimize.optimize import OptimizeWarning
 from scipy.optimize._lsq.least_squares import prepare_bounds
 from scipy.optimize.minpack import leastsq, _wrap_jac
@@ -107,20 +109,35 @@ def convert_pytorch_model_to_rational(model, rational_version='A', rational_cuda
         if len(list(childs)) > 0:
             sequential = nn.Sequential()
             for n, l in layer.named_children():
-                sequential.add_module(*_convert_layer(n, l, version=rational_version, cuda=rational_cuda))
+                sequential.add_module(*_convert_pytorch_layer(n, l, version=rational_version, cuda=rational_cuda))
             converted.add_module(name, sequential)
         else:
-            converted.add_module(*_convert_layer(name, layer, version=rational_version, cuda=rational_cuda))
+            converted.add_module(*_convert_pytorch_layer(name, layer, version=rational_version, cuda=rational_cuda))
     return converted
         
 
-def _convert_layer(name, layer, version, cuda):
+def _convert_pytorch_layer(name, layer, version, cuda):
     for activation in activations:
         if isinstance(layer, activation):
-            return f'Rational_{name}', Rational(version=version, approx_func=activations[activation], cuda=cuda)
+            return f'Rational_{name}', RationalPyTorch(version=version, approx_func=activations[activation], cuda=cuda)
     return name, layer
         
+    
+def convert_mxnet_model_to_rational(model, rational_version='A', rational_cuda=False):
+    converted = HybridSequential()
+    for name, layer in model._children.items():
+        childs = layer._children.items()
+        if len(list(childs)) > 0:
+            seq = HybridSequential()
+            for n, l in layer._children.items():
+                seq.add(_convert_mxnet_layer(layer, rational_version, rational_cuda))
+            converted.add(seq)
+        else:
+            converted.add(_convert_mxnet_layer(layer, rational_version, rational_cuda))
+    return converted
 
 
-
-
+def _convert_mxnet_layer(layer, version, cuda):
+    if isinstance(layer, Activation):
+        return RationalMxNet(version=version, approx_func='relu', cuda=cuda)
+    return layer
