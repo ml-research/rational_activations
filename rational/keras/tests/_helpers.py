91c70a41714da39a635c0ae0e7876de88a3fd83c
Nils Beck (Laptop) <nils.beck@protonmail.com>
2021-01-13 13:31:08 +0100

    added gpu/cpu distinction for tests

--- rational/keras/tests/_helpers.py
+++ rational/keras/tests/_helpers.py
@@ -20,24 +20,33 @@ def _activation(func, data):
     return np.array(func(data))
 
 
-def _test_template(version, approx_func, cuda):
+def _test_template(version: str, approx_func, cuda: bool):
     """
     compare the result of Rational activation function with expected result
 
     :param cuda: whether to execute on cuda
     :param version: which version of Rational activation function to test
     """
+    # configure the device
+    device_type = 'GPU' if cuda else 'CPU'
+    logical_devices = tf.config.list_logical_devices(device_type)
 
-    # instantiate tensor for testing purpose
-    test_data = [-2., -1, 0., 1., 2.]
-    test_tensor = tf.convert_to_tensor(np.array(test_data, np.float32), np.float32)
+    if len(logical_devices) == 0:
+        raise Exception('{} device not available for testing. \n Available devices: {}.'
+                        .format(device_type, str(tf.config.list_logical_devices())))
 
-    # instantiate expected results of activation function
-    expected_res = _activation(approx_func, test_data)
+    # execute the remaining test on the specified device
+    with tf.device(logical_devices[0].name):
+        # instantiate tensor for testing purpose
+        test_data = [-2., -1, 0., 1., 2.]
+        test_tensor = tf.convert_to_tensor(np.array(test_data, np.float32), np.float32)
 
-    # instantiate Rational activation function with specific version
-    trainable = False  # if version != 'D' else True
-    rational = Rational(approx_func=approx_func.__name__, version=version,
-                        cuda=cuda, trainable=trainable)(test_tensor).numpy()
+        # instantiate expected results of activation function
+        expected_res = _activation(approx_func, test_data)
 
-    assert np.all(np.isclose(rational, expected_res, atol=5e-02))
+        # instantiate Rational activation function with specific version
+        trainable = False  # if version != 'D' else True
+        rational = Rational(approx_func=approx_func.__name__, version=version,
+                            cuda=cuda, trainable=trainable)(test_tensor).numpy()
+
+        assert np.all(np.isclose(rational, expected_res, atol=5e-02))
