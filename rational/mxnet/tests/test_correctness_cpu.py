75c55947b10c8257a8f8ed19f5caced0ed6cbb44
Ting-Yu <tingyu.hsu@stud.tu-darmstadt.de>
2021-02-14 09:26:03 +0100

    update

--- rational/mxnet/tests/test_correctness_cpu.py
+++ rational/mxnet/tests/test_correctness_cpu.py
@@ -8,6 +8,7 @@ from mxnet import gluon
 import numpy as np
 
 
+
 # build a small neural net containing one Rational layer
 net = gluon.nn.HybridSequential()
 with net.name_scope():
@@ -20,9 +21,49 @@ def test():
     net(input_data)  # need to feed data for polynomial calculations before hyberdize!
     net.hybridize()  # running time: all ndarrays converted to symbols
 
-    # expected_res = LeakyReLU(data=input)
-    # result = fut(input).numpy()
-    # print('leakyrelu', expected_res)
-    # print('rational', result)
-    # assert np.all(np.isclose(expected_res, result, atol=5e-02))
-    pass
+def test_a_on_cpu_lrelu():
+    _test_template(version='A', approx_func=leaky_relu, cuda=CUDA)
+
+
+def test_b_on_cpu_lrelu():
+    _test_template(version='B', approx_func=leaky_relu, cuda=CUDA)
+
+
+def test_c_on_cpu_lrelu():
+    _test_template(version='C', approx_func=leaky_relu, cuda=CUDA)
+
+
+def test_d_on_cpu_lrelu():
+    _test_template(version='D', approx_func=leaky_relu, cuda=CUDA)
+
+
+def test_a_on_cpu_tanh():
+    _test_template(version='A', approx_func=tanh, cuda=CUDA)
+
+
+def test_b_on_cpu_tanh():
+    _test_template(version='B', approx_func=tanh, cuda=CUDA)
+
+
+def test_c_on_cpu_tanh():
+    _test_template(version='C', approx_func=tanh, cuda=CUDA)
+
+
+def test_d_on_cpu_tanh():
+    _test_template(version='D', approx_func=tanh, cuda=CUDA)
+
+
+def test_a_on_cpu_sigmoid():
+    _test_template(version='A', approx_func=sigmoid, cuda=CUDA)
+
+
+def test_b_on_cpu_sigmoid():
+    _test_template(version='B', approx_func=sigmoid, cuda=CUDA)
+
+
+def test_c_on_cpu_sigmoid():
+    _test_template(version='C', approx_func=sigmoid, cuda=CUDA)
+
+
+def test_d_on_cpu_sigmoid():
+    _test_template(version='D', approx_func=sigmoid, cuda=CUDA)
