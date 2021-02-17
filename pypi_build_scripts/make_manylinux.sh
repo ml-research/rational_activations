5e290dd6d17a9869a9b0c7337bda2da8ee453ae8
k4ntz <56230334+k4ntz@users.noreply.github.com>
2021-02-17 15:22:20 +0100

    Make the script work for all 3 cuda versions

--- pypi_build_scripts/make_manylinux.sh
+++ pypi_build_scripts/make_manylinux.sh
@@ -11,7 +11,7 @@ do
   esac
 done
 
-CUDA_V="cuda-10.2"
+CUDA_list=("cuda-10.1" "cuda-10.2" "cuda-11.0")
 
 ######### CHECKING ###############
 
@@ -27,45 +27,50 @@ torch_lib_list=(
   "/usr/local/lib64/python3.6/site-packages/torch/lib/"
   "/usr/local/lib/python3.7/site-packages/torch/lib/"
   "/usr/local/lib/python3.8/site-packages/torch/lib/")
-CUDA_LIB="/usr/local/$CUDA_V/lib64/"
-export CUDA_HOME="/usr/local/$CUDA_V"
-
-printf "Checking if python versions are correctly accessible and path to torch lib packages\n"
-for i in 0 1 2
+ 
+for j in 0 1 2
 do
-  PYTHON_V=${python_list[$i]}
-  TORCH_LIB=${torch_lib_list[$i]}
-  if [[ ! $TORCH_LIB =~ "$PYTHON_V" ]] || [ ! -d "$TORCH_LIB" ]
-  then
-    printf "Please provide a valid python torch lib path in \$TORCH_LIB\
-            \ne.g. /usr/lib64/python3.6/site-packages/torch/lib/\
-            \n$TORCH_LIB is not valid\n"
-    exit 1
-  fi
+  CUDA_V=${CUDA_list[$j]}
+  CUDA_LIB="/usr/local/$CUDA_V/lib64/"
+  export CUDA_HOME="/usr/local/$CUDA_V"
 
-  if [ -z "$CUDA_LIB" ] || [ ! -d "$CUDA_LIB" ]
-  then
-    printf "Please provide a valid cuda lib path in \$CUDA_LIB\
-            \ne.g. /usr/local/$CUDA_V/lib64/\n"
-    exit 1
-  fi
-done
-printf "All version correctly install with Rational's dependencies\n"
-unset PYTHON_V TORCH_LIB
+  printf "Checking if python versions are correctly accessible and path to torch lib packages\n"
+  for i in 0 1 2
+  do
+    PYTHON_V=${python_list[$i]}
+    TORCH_LIB=${torch_lib_list[$i]}
+    if [[ ! $TORCH_LIB =~ "$PYTHON_V" ]] || [ ! -d "$TORCH_LIB" ]
+    then
+      printf "Please provide a valid python torch lib path in \$TORCH_LIB\
+              \ne.g. /usr/lib64/python3.6/site-packages/torch/lib/\
+              \n$TORCH_LIB is not valid\n"
+      exit 1
+    fi
 
-######### CHECKING DONE  ###############
+    if [ -z "$CUDA_LIB" ] || [ ! -d "$CUDA_LIB" ]
+    then
+      printf "Please provide a valid cuda lib path in \$CUDA_LIB\
+              \ne.g. /usr/local/$CUDA_V/lib64/\n"
+      exit 1
+    fi
+  done
+  printf "All version correctly install with Rational's dependencies\n"
+  unset PYTHON_V TORCH_LIB
+
+  ######### CHECKING DONE  ###############
 
 
-# generate the wheels
-for i in 0 1 2
-do
-  PYTHON_V=${python_list[$i]}
-  TORCH_LIB=${torch_lib_list[$i]}
-  export LD_LIBRARY_PATH=/usr/local/lib:$TORCH_LIB:$CUDA_LIB  # for it to be able to find the .so files
-  rm -f /usr/local/cuda
-  ln -s /usr/local/$CUDA_LIB /usr/local/cuda
-  $PYTHON_V setup.py bdist_wheel
-  set -- "${@:2}"
-  source pypi_build_scripts/complete_wheel_repair.sh
-  $PYTHON_V setup.py clean
+  # generate the wheels
+  for i in 0 1 2
+  do
+    PYTHON_V=${python_list[$i]}
+    TORCH_LIB=${torch_lib_list[$i]}
+    export LD_LIBRARY_PATH=/usr/local/lib:$TORCH_LIB:$CUDA_LIB  # for it to be able to find the .so files
+    rm -f /usr/local/cuda
+    ln -s /usr/local/$CUDA_LIB /usr/local/cuda
+    $PYTHON_V setup.py bdist_wheel
+    set -- "${@:2}"
+    source pypi_build_scripts/complete_wheel_repair.sh
+    $PYTHON_V setup.py clean
+  done
 done
