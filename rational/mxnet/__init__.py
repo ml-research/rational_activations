0d22a7a5934c816442d097f16ccf5413f3f6239c
Nils Beck <nils.beck@protonmail.com>
2021-02-23 16:26:16 +0100

    fixed import errors. Added mxnet tests to pytest init

--- rational/mxnet/__init__.py
+++ rational/mxnet/__init__.py
@@ -1,4 +1,4 @@
 """
 This file imports code into the rational.mxnet package
 """
-from rationals import Rational
+from .rationals import Rational
--- rational/mxnet/tests/__init__.py
+++ rational/mxnet/tests/__init__.py
@@ -2,4 +2,4 @@
 This file imports content into the mxnet.tests directory
 """
 from rational.mxnet import Rational
-from helpers import _activation, _test_template
+from .helpers import _activation, _test_template
