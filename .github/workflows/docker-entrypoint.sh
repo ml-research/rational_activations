2ec933e9f26a3e67ea6062ae8965b4224d4b78f4
salva <salvaruehling@gmail.com>
2021-03-14 17:20:41 +0100

    hm++

--- .github/workflows/docker-entrypoint.sh
+++ .github/workflows/docker-entrypoint.sh
@@ -6,5 +6,7 @@ conda activate cicd_env_cuda10.1py3.7
 python3.7 -c "import sys; print('Python version:', sys.version)"
 python3.7 -c "import torch; print('CUDa version:', torch.version.cuda)"
 python3.7 -c "import cartopy; print('auasassss')"
+pip3.7 install airspeed pytest
+pip3.7 install -r requirements.txt
 python3.7  setup.py develop --user
 python3.7 -m pytest
