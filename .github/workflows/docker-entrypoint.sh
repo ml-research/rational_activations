2196088b98656e9be380f45bfbb503f87be27829
salva <salvaruehling@gmail.com>
2021-03-14 18:31:30 +0100

    move

--- .github/workflows/docker-entrypoint.sh
+++ .github/workflows/docker-entrypoint.sh
@@ -4,7 +4,6 @@ echo CUDA_HOME: $$CUDA_HOME
 pwd
 python3.7 -c "import sys; print('Python version:', sys.version)"
 python3.7 -c "import torch; print('CUDa version:', torch.version.cuda)"
-python3.7 -c "import cartopy; print('auasassss')"
 pip3.7 install airspeed pytest
 pip3.7 install -r requirements.txt
 python3.7 -c "import torch; print('CUDA version:', torch.version.cuda)"
