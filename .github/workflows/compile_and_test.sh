f82ea1167e6e8153aa43447601721e6551b57324
salva <salvaruehling@gmail.com>
2021-03-17 18:26:33 +0100

    check manylinux CI

---
+++ .github/workflows/compile_and_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd rational_activations
+git pull origin master
+./pypi_build_scripts/make_manylinux.sh
+ls wheelhouse
+python 3.6 -m pip install wheelhouse/cuda10.1/rational_activations-0.1.0-cp38-cp38-manylinux2014_x86_64.whl
+python --version
+python -c "import sys; print('Python version:', sys.version)"
+python -c "import torch; print('Cuda available:', torch.cuda.is_available())"
+python -c "import torch; print('CUDA version used by PyTorch:', torch.version.cuda)"
+nvcc --version
