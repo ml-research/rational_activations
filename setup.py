d7fc1a9dab17ae112c85b880543e299eb76f10f9
Quentin Delfosse <q.delfosse@gmail.com>
2021-04-14 11:13:05 +0200

    Uncompiled cuda version for old cuda versions

--- setup.py
+++ setup.py
@@ -25,95 +25,95 @@ def is_torch_cuda_available():
     force_cuda = os.getenv("FORCE_CUDA", "0") == "1"
     return force_cuda or torch_cuda_available()
 
-def generate_cpp_module(fname, degrees=degrees, versions=None):
-    file_content = airspeed.Template("""
-\#include <torch/extension.h>
-\#include <vector>
-\#include <iostream>
-
-#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
-
-
-#foreach ($vname in $versions)
-#if( $vname == 'D' )
-#set ($forward_header = 'const bool training, const unsigned long long iteration, torch::Tensor x, torch::Tensor n, torch::Tensor d')
-#set ($backward_header = 'const bool training, const unsigned long long iteration, torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d')
-#set ($forward_invocation = 'training, iteration, x, n, d')
-#set ($backward_invocation = 'training, iteration, grad_output, x, n, d')
-#else
-#set ($forward_header = 'torch::Tensor x, torch::Tensor n, torch::Tensor d')
-#set ($backward_header = 'torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d')
-#set ($forward_invocation = 'x, n, d')
-#set ($backward_invocation = 'grad_output, x, n, d')
-#end
-    #foreach ($degs in $degrees)
-	at::Tensor rational_cuda_forward_${vname}_$degs[0]_$degs[1]($forward_header);
-    std::vector<torch::Tensor> rational_cuda_backward_${vname}_$degs[0]_$degs[1]($backward_header);
-    #end
-
-
-    #foreach ($degs in $degrees)
-    at::Tensor rational_forward_${vname}_$degs[0]_$degs[1]($forward_header) {
-        CHECK_INPUT(x);
-        CHECK_INPUT(n);
-        CHECK_INPUT(d);
-
-        return rational_cuda_forward_${vname}_$degs[0]_$degs[1]($forward_invocation);
-    }
-    std::vector<torch::Tensor> rational_backward_${vname}_$degs[0]_$degs[1]($backward_header) {
-        CHECK_INPUT(grad_output);
-        CHECK_INPUT(x);
-        CHECK_INPUT(n);
-        CHECK_INPUT(d);
-
-        return rational_cuda_backward_${vname}_$degs[0]_$degs[1]($backward_invocation);
-    }
-    #end
-#end
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-#foreach ($degs in $degrees)
-    #foreach ($vname in $versions)
-    m.def("forward_${vname}_$degs[0]_$degs[1]", &rational_forward_${vname}_$degs[0]_$degs[1], "Rational forward ${vname}_$degs[0]_$degs[1]");
-    m.def("backward_${vname}_$degs[0]_$degs[1]", &rational_backward_${vname}_$degs[0]_$degs[1], "Rational backward ${vname}_$degs[0]_$degs[1]");
-    #end
-#end
-}
-    """)
-
-    content = file_content.merge(locals())
-
-    with open(fname, "w") as text_file:
-        text_file.write(content)
-
-
-def generate_cpp_kernels_module(fname, degrees=degrees, template_contents=None):
-    degrees = [[e[0], e[1], max(e[0], e[1])] for e in degrees]
-
-    template = """
-\#include <torch/extension.h>
-\#include <ATen/cuda/CUDAContext.h>
-\#include <cuda.h>
-\#include <cuda_runtime.h>
-\#include <vector>
-\#include <stdlib.h>
-
-\#include <curand.h>
-\#include <curand_kernel.h>
-\#include <curand_philox4x32_x.h>
-
-constexpr uint32_t THREADS_PER_BLOCK = 512;
-"""
-
-
-    file_content = airspeed.Template(template + template_contents)
-
-    content = file_content.merge(locals())
-
-    with open(fname, "w") as text_file:
-        text_file.write(content)
+# def generate_cpp_module(fname, degrees=degrees, versions=None):
+#     file_content = airspeed.Template("""
+# \#include <torch/extension.h>
+# \#include <vector>
+# \#include <iostream>
+#
+# #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
+# #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
+# #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+#
+#
+# #foreach ($vname in $versions)
+# #if( $vname == 'D' )
+# #set ($forward_header = 'const bool training, const unsigned long long iteration, torch::Tensor x, torch::Tensor n, torch::Tensor d')
+# #set ($backward_header = 'const bool training, const unsigned long long iteration, torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d')
+# #set ($forward_invocation = 'training, iteration, x, n, d')
+# #set ($backward_invocation = 'training, iteration, grad_output, x, n, d')
+# #else
+# #set ($forward_header = 'torch::Tensor x, torch::Tensor n, torch::Tensor d')
+# #set ($backward_header = 'torch::Tensor grad_output, torch::Tensor x, torch::Tensor n, torch::Tensor d')
+# #set ($forward_invocation = 'x, n, d')
+# #set ($backward_invocation = 'grad_output, x, n, d')
+# #end
+#     #foreach ($degs in $degrees)
+# 	at::Tensor rational_cuda_forward_${vname}_$degs[0]_$degs[1]($forward_header);
+#     std::vector<torch::Tensor> rational_cuda_backward_${vname}_$degs[0]_$degs[1]($backward_header);
+#     #end
+#
+#
+#     #foreach ($degs in $degrees)
+#     at::Tensor rational_forward_${vname}_$degs[0]_$degs[1]($forward_header) {
+#         CHECK_INPUT(x);
+#         CHECK_INPUT(n);
+#         CHECK_INPUT(d);
+#
+#         return rational_cuda_forward_${vname}_$degs[0]_$degs[1]($forward_invocation);
+#     }
+#     std::vector<torch::Tensor> rational_backward_${vname}_$degs[0]_$degs[1]($backward_header) {
+#         CHECK_INPUT(grad_output);
+#         CHECK_INPUT(x);
+#         CHECK_INPUT(n);
+#         CHECK_INPUT(d);
+#
+#         return rational_cuda_backward_${vname}_$degs[0]_$degs[1]($backward_invocation);
+#     }
+#     #end
+# #end
+#
+# PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+# #foreach ($degs in $degrees)
+#     #foreach ($vname in $versions)
+#     m.def("forward_${vname}_$degs[0]_$degs[1]", &rational_forward_${vname}_$degs[0]_$degs[1], "Rational forward ${vname}_$degs[0]_$degs[1]");
+#     m.def("backward_${vname}_$degs[0]_$degs[1]", &rational_backward_${vname}_$degs[0]_$degs[1], "Rational backward ${vname}_$degs[0]_$degs[1]");
+#     #end
+# #end
+# }
+#     """)
+#
+#     content = file_content.merge(locals())
+#
+#     with open(fname, "w") as text_file:
+#         text_file.write(content)
+#
+#
+# def generate_cpp_kernels_module(fname, degrees=degrees, template_contents=None):
+#     degrees = [[e[0], e[1], max(e[0], e[1])] for e in degrees]
+#
+#     template = """
+# \#include <torch/extension.h>
+# \#include <ATen/cuda/CUDAContext.h>
+# \#include <cuda.h>
+# \#include <cuda_runtime.h>
+# \#include <vector>
+# \#include <stdlib.h>
+#
+# \#include <curand.h>
+# \#include <curand_kernel.h>
+# \#include <curand_philox4x32_x.h>
+#
+# constexpr uint32_t THREADS_PER_BLOCK = 512;
+# """
+#
+#
+#     file_content = airspeed.Template(template + template_contents)
+#
+#     content = file_content.merge(locals())
+#
+#     with open(fname, "w") as text_file:
+#         text_file.write(content)
 
 if is_torch_cuda_available():
     version_names = []
@@ -168,19 +168,19 @@ setup(
         "License :: OSI Approved :: Apache Software License"
     ],
     install_requires=requirements,
-    ext_modules=[
-        CUDAExtension('rational.cuda', [
-            'rational/_cuda/rational_cuda.cpp',
-            'rational/_cuda/rational_cuda_kernels.cu',
-        ],
-        extra_compile_args={'cxx': [],
-            'nvcc': ['-gencode=arch=compute_60,code="sm_60,compute_60"', '-lineinfo']
-        }
-    ),
-    ] if is_torch_cuda_available() else [],
-    cmdclass={
-        'build_ext': BuildExtension,
-        'clean': clean_all
-    },
-    setup_requires=['airspeed'],
+    # ext_modules=[
+    #     CUDAExtension('rational.cuda', [
+    #         'rational/_cuda/rational_cuda.cpp',
+    #         'rational/_cuda/rational_cuda_kernels.cu',
+    #     ],
+    #     extra_compile_args={'cxx': [],
+    #         'nvcc': ['-gencode=arch=compute_60,code="sm_60,compute_60"', '-lineinfo']
+    #     }
+    # ),
+    # ] if is_torch_cuda_available() else [],
+    # cmdclass={
+    #     'build_ext': BuildExtension,
+    #     'clean': clean_all
+    # },
+    setup_requires=[''],
     python_requires='>=3.5.0',)
