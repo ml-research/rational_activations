ba3a0259a84a50388d58d5071c5d8a66cb271462
Quentin Delfosse <q.delfosse@gmail.com>
2021-08-10 11:59:15 +0200

    Fixed the compilation and made it compatible with last PEP

--- pypi_build_scripts/make_manylinux.sh
+++ pypi_build_scripts/make_manylinux.sh
@@ -17,24 +17,27 @@ if [ -d "wheelhouse" ]; then
   mv wheelhouse wheelhouse_save
 fi
 
-CUDA_list=("cuda-10.2" "cuda-11.0")
-CUDA_COMP_list=("cu102" "cu110")
+CUDA_list=("cuda-11.1")
+CUDA_COMP_list=("cu111") 
+# CUDA_list=("cuda-10.2" "cuda-11.0")
+# CUDA_COMP_list=("cu102" "cu110")
 ######### CHECKING ###############
 
 
-if [ ! python3.7 -c "" &> /dev/null || ! python3.8 -c "" &> /dev/null ]; then
+if [ ! python3.7 -c "" &> /dev/null || ! python3.8 -c "" &> /dev/null || ! python3.9 -c "" &> /dev/null ]; then
   printf "python3.7 and/or python3.8 not installed\n Installing...\n"
   bash pypi_build_scripts/install_all_python.sh
 fi
 
 
-python_list=(python3.6 python3.7 python3.8)
+python_list=(python3.6 python3.7 python3.8 python3.9)
 torch_lib_list=(
   "/usr/local/lib64/python3.6/site-packages/torch/lib/"
   "/usr/local/lib/python3.7/site-packages/torch/lib/"
-  "/usr/local/lib/python3.8/site-packages/torch/lib/")
+  "/usr/local/lib/python3.8/site-packages/torch/lib/"
+  "/usr/local/lib/python3.9/site-packages/torch/lib/")
  
-for j in 0 1
+for j in 0
 do
   CUDA_V=${CUDA_list[$j]}
   CUDA_CP=${CUDA_COMP_list[$j]}
@@ -49,10 +52,10 @@ do
      echo "Could not create symlink for $CUDA_HOME -> $CUDA_P"
   fi
   ln -s $CUDA_HOME $CUDA_P
-  sed -i "s/name='rational-activations'.*/name='rational-activations-$CUDA_CP'/" setup.py
+  # sed -i "s/name='rational-activations'.*/name='rational-activations-$CUDA_CP'/" setup.py
 
   printf "Checking if python versions are correctly accessible and path to torch lib packages\n"
-  for i in 0 1 2
+  for i in 0 1 2 3
   do
     PYTHON_V=${python_list[$i]}
     TORCH_LIB=${torch_lib_list[$i]}
@@ -78,10 +81,16 @@ do
 
 
   # generate the wheels
-  for i in 0 1 2
+  for i in 0 1 2 3
   do
     PYTHON_V=${python_list[$i]}
     TORCH_LIB=${torch_lib_list[$i]}
+    CUDA_V=${CUDA_list[$j]}
+    CUDA_CP=${CUDA_COMP_list[$j]}
+    CUDA_LIB="/usr/local/$CUDA_V/lib64/"
+    export CUDA_HOME="/usr/local/$CUDA_V"
+    CUDA_P="/usr/local/cuda"
+    printf "\n\n\n$PYTHON_V\n$TORCH_LIB\n$CUDA_LIB\n\n\n"
     export LD_LIBRARY_PATH=/usr/local/lib:$TORCH_LIB:$CUDA_LIB  # for it to be able to find the .so files
     rm -f /usr/local/cuda
     ln -s /usr/local/$CUDA_V /usr/local/cuda
@@ -91,5 +100,5 @@ do
     $PYTHON_V setup.py clean
   done
 
-  sed -i "s/name='rational-activations-$CUDA_CP'.*/name='rational-activations'/" setup.py
+  # sed -i "s/name='rational-activations-$CUDA_CP'.*/name='rational-activations'/" setup.py
 done
